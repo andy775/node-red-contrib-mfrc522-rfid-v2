e15e9fbe7c63f670bd0d81871504eb22c53c0fcb
Frederic Bonjour <fred@habilebill.fr>
2020-10-14 11:42:29 +0200

    Added 'CS pin' and 'data block' options.

--- rc522.js
+++ rc522.js
@@ -11,7 +11,7 @@ module.exports = function(RED) {
       clock:  23, // pin number of SCLK
       mosi:   19, // pin number of MOSI
       miso:   21, // pin number of MISO
-      client: 24  // pin number of CS
+      client: config.CSpin || 24 // pin number of CS
     });
     const mfrc522 = new Mfrc522(softSPI).setResetPin(22);
 
@@ -25,49 +25,75 @@ module.exports = function(RED) {
         node.status({});
       }
       mfrc522.reset();
+
+
       let response = mfrc522.findCard();
-      if (response.status) {
-        const bitSize = response.bitSize;
-        response = mfrc522.getUid();
-        if (response.status) {
-          const uid_array = response.data;
-          let uid = ""
-          for(var i = 0; i < uid_array.length;i++){
-                if(i > 0){
-                  uid += ":"
-                }
-                if(uid_array[i] < 10){
-                  uid += "0"
-                }
-                uid += uid_array[i].toString(16).toUpperCase()
-          }
-            if  (uid != last_uid || timestamp > (last_time + node.blockedFor * 1000)) {
-              node.status({
-                fill: "green",
-                shape: "dot",
-                text: uid
-              });
-              last_time = timestamp;
-              last_uid = uid;
-               node.send({"payload": {
-                 "uid": uid,
-                 "timestamp": timestamp,
-//                 "uid_array": uid_array,
-                 "bitSize": bitSize
-               }});
-
-  node.log("New Card detected, CardType: " + bitSize);
-  node.log("UID: " + uid);
-
-          }
-        } else {
-         node.log("Card read error!");
-         return;
-        }
-      } else {
+      if (!response.status) {
         return;
+      }
+      const bitSize = response.bitSize;
+      response = mfrc522.getUid();
+      if (!response.status) {
+        return;
+      }
+
+      const uid_array = response.data;
+      let uid = ""
+      for(var i = 0; i < uid_array.length;i++) {
+        if (i > 0) {
+          uid += ":";
+        }
+        if (uid_array[i] < 10) {
+          uid += "0";
+        }
+        uid += uid_array[i].toString(16).toUpperCase();
+      }
+
+      if (uid != last_uid || timestamp > (last_time + node.blockedFor * 1000)) {
+        let data = null;
+        if (config.dataBlockAddr) {
+          data = '';
+          // Select the scanned card
+          mfrc522.selectCard(uid_array);
+          // This is the default key for authentication
+          const key = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
+          // Authenticate on Block 8 with key and uid
+          if (!mfrc522.authenticate(config.dataBlockAddr, key, uid_array)) {
+            node.status({
+              fill: "red",
+              shape: "dot",
+              text: "Authentication error."
+            });
+            console.log("Authentication Error");
+            return;
+          }
+          const len = config.dataBlockLength || 4;
+          const rawdata = mfrc522.getDataForBlock(config.dataBlockAddr);
+          for (let i=0; i<len; i++) {
+            data += String.fromCharCode(rawdata[i]);
+          }
+          mfrc522.stopCrypto();
+        }
+
+        node.status({
+          fill: "green",
+          shape: "dot",
+          text: uid
+        });
+
+        last_time = timestamp;
+        last_uid = uid;
+        node.send({"payload": {
+          uid,
+          data,
+          timestamp,
+          bitSize
+        }});
+
+        node.log("New Card detected, CardType: " + bitSize);
+        node.log("UID: " + uid);
       }
     }, refreshRate * 1000);
   }
-  RED.nodes.registerType("rc522",rc522Node);
+  RED.nodes.registerType("rc522-extra",rc522Node);
 }
